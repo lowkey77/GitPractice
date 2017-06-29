a1c4244edc1f22730541339864f177d9f929d666
reggie <lowkey77@gmail.com>
2017-06-29 09:52:01 -0600

    Second commit with added printline

--- GitPractice/src/practice/git/HelloGit.java
+++ GitPractice/src/practice/git/HelloGit.java
@@ -5,5 +5,7 @@ public class HelloGit
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello GIT!");
+		
+		System.out.println("Pushed initial commit.  Here is the second.");
 	}
 }
